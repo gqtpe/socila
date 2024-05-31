ab46cb7f7020749eeb67826782b593317bd576e6
gqtpe <work.temirtas@gmail.com>
2024-05-31 20:25:38 +0500

    socila v01: --app structure --outlet: where children are appear

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.scss';
+import Header from "./components/Header/Header";
+import NavBar from "./components/NavBar/NavBar";
+import {Outlet} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-        hellow world!
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <NavBar/>
+            <main>
+                <Outlet/>
+            </main>
+        </div>
+    );
 }
+
 
 export default App;
